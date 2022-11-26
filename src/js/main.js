e77d54d7e2dd5356df72e7c6d396027ff7773093
Josh Watley <watleyjosh@gmail.com>
2022-11-26 18:12:31 +0000

    updating copy and button functions

--- src/js/main.js
+++ src/js/main.js
@@ -93,3 +93,11 @@ function emailValidation(){
   errorMessage.style.display = "block"
   return false;
 }
+
+
+// Scroll to top
+
+function goTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
